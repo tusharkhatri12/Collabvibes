044fcad321b20192ef0f316f049c6f4508741a71
Tushar Khatri <tusharkhatri365@gmail.com>
2026-03-11 21:10:11 +0530

    initial deploy

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/collabrativesolutions");
+
+app.get("/", (req,res)=>{
+  res.send("collabrativesolutions API running");
+});
+
+app.listen(5000,()=>{
+  console.log("Server running on port 5000");
+});
